97f44f9ac85bfb28f4bd6fe4bdec6e05386d35c9
d-McGill <dmcgillz@icloud.com>
2022-01-28 14:33:57 +0000

    -c

--- application/views/createBooking.php
+++ application/views/createBooking.php
@@ -2,29 +2,33 @@
 <div class="container">
    <div class="row">
      <h1>Create a Booking request</h1>
-     <p>Simply select a date and anytimes you would like to have your appiontment, then hit book now</p>
+     <p>Simply select a service, date and anytimes you would like to have your appiontment, then hit submit request</p>
      <hr/>
-     <div class="input-group">
-       <h3>Service</h3>
-       <select class="selectpicker" name"service" data-width="100%;">
-         <option value="1">One</option>
-         <option value="2">Two</option>
-         <option value="3">Three</option>
-       </select>
+     <div class="col-sm-6 my-4">
+       <div class="input-group">
+         <h3>Service</h3>
+         <select class="selectpicker" name="service" data-width="100%" data-dropup-auto="false">
+           <option value="1">One</option>
+           <option value="2">Two</option>
+           <option value="3">Three</option>
+         </select>
+       </div>
+       <br/>
+       <h3>Select Date</h3>
+          <div id="pick-inline" class="my-4"></div>
+          <input type="text" id="input-inline" placeholder="Selected Date" name="date">
+
      </div>
-     <div class="col-sm-6">
-       <h3>Date</h3>
-       <input type="text" id="input-inline" placeholder="Select a date" name="date">
-       <div id="pick-inline"></div>
-     </div>
-     <div class="col-sm-6">
+
+     <div class="col-sm-6 my-4">
        <div class="input-group">
           <h3>Times</h3>
-         <select id="timeLoop" class="selectpicker" multiple name="times" data-width="100%"></select>
+         <select id="timeLoop" class="selectpicker" multiple name="times" data-width="100%" data-max-options="4" ></select>
        </div>
-     </div>
-       <button class="btn btn-primary" type="button">Button</button>
 
+    </div>
+     <button type="button" class="btn btn-primary btn-lg btn-block">Submit Request</button>
+ </div>
 
      <script>
      window.addEventListener("load", () => {
@@ -34,7 +38,7 @@
        });
      });
      </script>
- </div>
+
    </div>
    </div>
 </div>
--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -12,5 +12,5 @@
   <script src="<?php echo base_url("public/js/popper.min.js")?>"></script>
   <script src="<?php echo base_url("public/js/bootstrap.min.js")?>"></script>
   <script src="<?php echo base_url("public/js/bootstrap-select.min.js")?>"></script>
-    <script src="<?php echo base_url("public/js/datepicker.js")?>"></script>
+  <script src="<?php echo base_url("public/js/datepicker.js")?>"></script>
 </html>
