69f02e853a384deb7a66ac146b9a6d60026b5765
d-McGill <dmcgillz@icloud.com>
2022-01-28 14:49:04 +0000

    -c

--- database.sql
+++ database.sql
@@ -1,10 +1,21 @@
 DROP Users
 
 CREATE TABLE users (
-  GUID int(11) PRIMARY KEY AUTO_INCREMENT,
+  userID int(11) PRIMARY KEY AUTO_INCREMENT,
   name varchar(255),
   email varchar(255),
   password varchar(255),
   address varchar(255),
   postcode varchar(255)
+);
+
+
+CREATE TABLE bookings(
+  bookingID int(11) PRIMARY KEY AUTO_INCREMENT,
+  userID int(11),
+  FOREIGN KEY (userID) REFERENCES users(userID),
+  service varchar(255),
+  dayDate date(),
+  dayTimes time(),
+  status varchar(255)
 );
