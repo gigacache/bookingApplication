ada907cbad49f73a48432fdb197e8332bb0b57ce
d-McGill <77538732+d-McGill@users.noreply.github.com>
2022-03-09 12:59:56 +0000

    update

--- application/views/index.php
+++ application/views/index.php
@@ -39,6 +39,7 @@ if($this->session->login==TRUE){redirect('dashboard');}?>
         <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
         <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
         </svg>
+        <span>Register</span>
       </a>
       </li>
     </ul>
@@ -107,9 +108,7 @@ if($this->session->login==TRUE){redirect('dashboard');}?>
 		 </div>
 	 </div>
 	</div>
-
-
-	</body>
+</body>
 
 
 	  <script src="<?php echo base_url("public/js/jquery-3.3.1.slim.min.js");?>"></script>
