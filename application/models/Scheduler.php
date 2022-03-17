36df401fa65cad775320b4a4902e515c9df1cadb
d-McGill <77538732+d-McGill@users.noreply.github.com>
2022-03-17 17:17:30 +0000

    first rule implmented

--- application/models/Scheduler.php
+++ application/models/Scheduler.php
@@ -28,35 +28,55 @@ public function sortSchedule($obj){
   print_r($obj->result_array());
   echo "<br/>";
   $startTime = '08:00';
+  $count=0;
   $new_time = strtotime($startTime);
     foreach ($obj->result_array() as $row){
-      
+
       $seconds2add =0;
         if($row['timeOfDay'] == 'Morning'){
 
           if($row['service'] == 1){
-            $seconds2add = 30*60;}
+            $seconds2add = 1800;}
 
           if($row['service'] == 2){
-            $seconds2add = 45*60;}
+            $seconds2add = 1800;}
 
           if($row['service'] == 3){
-            $seconds2add = 60*60;}
+            $seconds2add = 1800;}
 
           $new_time+=$seconds2add;
 
+          echo '<br/>';
+          echo 'new time: ';
+          echo date('h:i',$new_time);
+          echo '<br/>';
 
+          $dataTime = date('h:i',$new_time);
 
-          if (!DateTime::createFromFormat('H:i:', '2017-10-08 17:23')) {
-              echo date('H:i', strtotime($startTime));
-              echo '<br/>';
-              echo date('H:i', strtotime($new_time));
-            }
-
-        //  $this->addToScheduler($row['userID'],$row['bookingDate'], $row['service'],$time, $new_time);
+          if($count == 1){
+          $dataTim = date('h:i',$startTime);}
+          else{
+              $dataTim = '08:00';
+}
+          $this->addToScheduler($row['userID'],$row['bookingDate'], $row['service'],$dataTim, $dataTime);
 
           $startTime =+ $new_time;
+$count++;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       }
 }
